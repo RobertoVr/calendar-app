196c786c980e453364aacf2e1e5611c684f666ad
Roberto Valdez <robert.tito94@gmail.com>
2020-11-18 14:39:41 -0600

    agregar calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { Navbar } from '../ui/Navbar';
+
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const myEventsList = [
+    {
+        title: 'Cumple',
+        start: moment().toDate(),
+        end: moment().add(2, 'hours').toDate(),
+        bgcolor: '#fafafafa'
+    }
+]
 
 export const CalendarScreen = () => {
     return (
-        <div>
-            calendar works
+        <div className="calendar-screen">
+            <Navbar />
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+            />
         </div>
     )
 }
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-brand">
+                Roberto
+            </span>
+            <button className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
+                <span> Salir</span>
+            </button>
+        </div>
+    )
+}
